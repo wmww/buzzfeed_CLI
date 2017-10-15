ad9ea88a59fb5f5edb46857eab3638d3496e15fa
William W Wold <wilcoze@gmail.com>
2017-10-15 16:42:50 -0700

    added stuff about API key

---
+++ readme.md
@@ -0,0 +1,5 @@
+# BuzzFeed CLI
+
+work in progress
+
+__to use, you need your own News API API key. Go to [https://newsapi.org/](https://newsapi.org/), register and put your key in a file called 'news_api_key.txt' in the project root. The file should only contain the key surrounded by double quotes.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 extern crate hyper;
+use hyper::Client;
 
 fn main() {
+    let news_api_key = include!("../news_api_key.txt");
+    //let client = Client::new();
+    println!("{}", news_api_key);
+    //let response =
+    //client.get("https://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey={API_KEY}")
+    //.send()
+    //.expect("Request failed");
     println!("Hello, world!");
 }
