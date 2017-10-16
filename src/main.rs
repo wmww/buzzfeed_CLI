1e8da788c4d708623c1e076989a1645b3c4b01bd
William W Wold <wilcoze@gmail.com>
2017-10-15 20:26:14 -0700

    tried to add abstraction, shit is fucked up now

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,51 @@ use hyper::Client;
 use tokio_core::reactor::Core;
 //use std::{thread, time};
 
+struct HttpThing {
+    core: Core,
+    client: Client<hyper::client::HttpConnector>,
+}
+
+struct HttpResponse {
+    status: u32,
+    body: String
+}
+
+impl HttpThing {
+
+    fn new() -> HttpThing {
+        let mut core = Core::new().expect("something fucked up");
+        let client = Client::new(&core.handle());
+        HttpThing {core, client}
+    }
+
+    fn get_blocking(&mut self, uri: &str) -> HttpResponse {
+        let uri = uri.parse().expect("uri parsing fucked up");
+        let mut status = 0;
+
+        let work = self.client.get(uri).and_then(|res| {
+            let mut body = String::new();
+            res.body().for_each(|chunk| {
+                let cow = String::from_utf8_lossy(chunk.as_ref());
+                body.push_str(&cow.into_owned());
+                //println!("{}\n\n", String::from_utf8_lossy(chunk.as_ref()));
+                //body.push(&chunk);
+                //io::stdout()
+                //    .write_all(&chunk)
+                //    .map_err (From::from)
+                Ok(())
+            })
+        });
+        self.core.run(work).expect("something fucked up");
+        HttpResponse {status, body}
+    }
+}
+
 fn main() {
     let news_api_key = include!("../news_api_key.txt");
     let headlinesURL = format!("http://newsapi.org/v1/articles?source=buzzfeed&sortBy=top&apiKey={}", news_api_key);
-    let mut core = Core::new().expect("something fucked up");
-    let client = Client::new(&core.handle());
-    let uri = headlinesURL.parse().expect("something fucked up");
-    let work = client.get(uri).and_then(|res| {
+    let http = HttpThing::new();
+    /*let work = client.get(uri).and_then(|res| {
         println!("Response: {}", res.status());
         let body: String;
         res.body().for_each(|chunk| {
@@ -26,5 +64,6 @@ fn main() {
             Ok(())
         })
     });
-    core.run(work).expect("something fucked up");
+    core.run(work).expect("something fucked up");*/
+    println!("{}", http.get_blocking(&headlinesURL).body);
 }
